ce182cd3da3da8bd02f0942c5487ce0ac47511f9
Ashley de Witte <ashley.dewitte@mail.utoronto.ca>
2022-10-31 16:59:45 -0400

    Added GroupRepoInt (GroupRepo interface) that specifics the methods GroupDataAccess must implement.

---
+++ src/main/Databases/GroupRepoInt.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public interface GroupRepoInt {
+    boolean addGroup();
+
+    boolean removeGroup();
+
+    // Return type subject to change
+    List<String> getGroupData();
+
+    boolean addRanGroupID();
+
+    boolean removeRanGroupID();
+
+    List<Integer> getRanGroupIDs();
+
+
+
+}
