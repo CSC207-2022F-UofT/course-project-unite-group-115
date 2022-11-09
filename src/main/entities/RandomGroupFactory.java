ac239f3e255a560eb0c680dbbed2a9405b65526f
Ashley de Witte <ashley.dewitte@mail.utoronto.ca>
2022-11-09 12:11:55 -0500

    Created the GroupFactory interface.
    Created RandomGroupFactory class that implements the GroupFactory interface.

---
+++ src/main/entities/RandomGroupFactory.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class RandomGroupFactory implements GroupFactory {
+
+    @Override
+    public Group createNewGroup(String name, List<String> interests, List<User> members) {
+        return new Group(name, interests, members, true);
+    }
+
+    @Override
+    public Group recreateExisitingGroup(String name, List<String> interests, List<User> members, String id) {
+        return new Group(name, interests, members, true, id);
+    }
+}
