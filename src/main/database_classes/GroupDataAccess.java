275584ba067e47e25dcff5abcac38f4de82d7693
an1381h <amir.nasr@mail.utoronto.ca>
2022-11-28 00:41:24 -0500

    Made every method in GroupRepoInt

--- src/main/database_classes/GroupDataAccess.java
+++ src/main/database_classes/GroupDataAccess.java
@@ -2,6 +2,48 @@ package database_classes;
 
 import general_group.use_case.GroupRepoInt;
 
+import java.util.List;
+import java.util.Map;
+
 public class GroupDataAccess implements GroupRepoInt {
 
+    @Override
+    public void addGroup(GroupRepoDsRequestModel requestModel) {
+
+    }
+
+    @Override
+    public void removeGroup(String groupID) {
+
+    }
+
+    @Override
+    public Map<String, Object> getGroupData(String groupID) {
+        return null;
+    }
+
+    @Override
+    public void addUserToGroup(String user, String groupID) {
+
+    }
+
+    @Override
+    public void removeUserFromGroup(String user, String groupID) {
+
+    }
+
+    @Override
+    public void addInterestsToGroup(String interest, String groupID) {
+
+    }
+
+    @Override
+    public void removeInterestsfromGroup(String interest, String groupID) {
+
+    }
+
+    @Override
+    public List<String> getGroups() {
+        return null;
+    }
 }
--- src/main/general_group/use_case/GroupRepoInt.java
+++ src/main/general_group/use_case/GroupRepoInt.java
@@ -1,6 +1,27 @@
 package general_group.use_case;
 
+import database_classes.GroupRepoDsRequestModel;
+import java.util.Map;
+import java.util.List;
+
 public interface GroupRepoInt {
+
+    void addGroup(GroupRepoDsRequestModel requestModel);
+
+    void removeGroup(String groupID);
+
+    Map<String, Object> getGroupData(String groupID);
+
+    void addUserToGroup(String user, String groupID);
+
+    void removeUserFromGroup(String user, String groupID);
+
+    void addInterestsToGroup(String interest, String groupID);
+
+    void removeInterestsfromGroup(String interest, String groupID);
+
+    List<String> getGroups();
+
 
 
 }
