919894a1528f2f1e3450be55b317c16d6551ece7
aurorali17 <aurorabing.li@mail.utoronto.ca>
2022-11-28 03:05:37 -0500

    complete user report database and user cases part. will carry on tests later.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,5 @@
 import Interface.UserReporterController;
+import frameworks_and_drivers.LoggedinScreen;
 import frameworks_and_drivers.Report_First_Screen;
 import frameworks_and_drivers.User_Report_Screen;
 
@@ -7,7 +8,7 @@ import javax.swing.*;
 public class main {
     public static void main(String[] args) {
 
-        JFrame application2 = new Report_First_Screen();
+        JFrame application2 = new LoggedinScreen("Aurora");
         application2.pack();
         application2.setVisible(true);
 
