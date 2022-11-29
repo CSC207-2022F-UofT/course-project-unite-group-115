a4ac4155e2dba006e2a3c48c8a1a23ae10599ea2
an1381h <amir.nasr@mail.utoronto.ca>
2022-11-28 23:18:13 -0500

    Created the presenter

--- src/main/general_group/interface_adapters/GeneralGroupCreatePresenter.java
+++ src/main/general_group/interface_adapters/GeneralGroupCreatePresenter.java
@@ -3,14 +3,19 @@ package general_group.interface_adapters;
 import general_group.use_case.GeneralGroupCreateDsResponseModel;
 import general_group.use_case.GeneralGroupCreateOutputBoundary;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class GeneralGroupCreatePresenter implements GeneralGroupCreateOutputBoundary {
     @Override
-    public GeneralGroupCreateDsResponseModel prepareSuccessView(GeneralGroupCreateDsResponseModel group) {
-        return null;
+    public GeneralGroupCreateDsResponseModel prepareSuccessView(GeneralGroupCreateDsResponseModel responseModel) {
+        LocalDateTime responseTime = LocalDateTime.parse(responseModel.getCreationTime());
+        responseModel.setCreationTime(responseTime.format(DateTimeFormatter.ofPattern("hh:mm:ss")));
+        return responseModel;
     }
 
     @Override
     public GeneralGroupCreateDsResponseModel prepareFailView(String error) {
-        return null;
+        throw new GenGroupCreationFailed(error);
     }
 }
--- src/main/general_group/use_case/GeneralGroupCreateOutputBoundary.java
+++ src/main/general_group/use_case/GeneralGroupCreateOutputBoundary.java
@@ -2,7 +2,7 @@ package general_group.use_case;
 
 public interface GeneralGroupCreateOutputBoundary {
 
-    GeneralGroupCreateDsResponseModel prepareSuccessView(GeneralGroupCreateDsResponseModel group);
+    GeneralGroupCreateDsResponseModel prepareSuccessView(GeneralGroupCreateDsResponseModel responseModel);
 
     GeneralGroupCreateDsResponseModel prepareFailView(String error);
 }
