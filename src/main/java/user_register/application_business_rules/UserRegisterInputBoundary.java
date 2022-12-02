2deccc866f4513ad94ab8bf853bc19626812892f
Kushagra Raghuvanshi <kushagra2125@gmail.com>
2022-12-01 22:16:01 -0500

    Starting writing javadocs

--- src/main/java/user_register/application_business_rules/UserRegisterInputBoundary.java
+++ src/main/java/user_register/application_business_rules/UserRegisterInputBoundary.java
@@ -1,6 +1,6 @@
 package user_register.application_business_rules;
 
 public interface UserRegisterInputBoundary {
-    // Adding javadocs
+    // Adding javadocs for UserRegister Feature
     UserRegisterResponseModel create(UserRegisterRequestModel requestModel);
 }
