558717a62dd4d916fb34d58caaa801328cb59c73
Kushagra Raghuvanshi <kushagra2125@gmail.com>
2022-12-05 18:11:21 -0500

    Added Tests for User Register use case.

--- src/test/java/entities/UserTest.java
+++ src/test/java/entities/UserTest.java
@@ -1,4 +1,12 @@
 package entities;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 public class UserTest {
+    @Test
+    void given123Password_whenPasswordIsNotValid_thenIsFalse() {
+        User user = new User("Kushagra", "abcd");
+
+        assertFalse(user.passwordIsValid());
+    }
 }
